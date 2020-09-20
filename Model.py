a43860d514af79d2fab2dba8c97f9118f1fa35b7
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-09-19 23:12:07 -0700

    Add classes and outline pipeline flow

---
+++ Model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+class Model:
+    def __init__(self):
+        pass
+
+    # crunch some numbers, return dataframe
+    def model_fn(self, df):
+        pass
--- lambda_pipeline.py
+++ lambda_pipeline.py
@@ -4,44 +4,14 @@ import os
 import boto3
 import boto3.session
 
-# this will be used to access Aurora's Data API
-from sqlalchemy import create_engine
-# awswrangler provides convenient methods for Pandas to AWS DB
-import awswrangler as wr
-
-import pandas as pd
-
-from nba_api.stats.static import players
-
-
-def db(session):
-    rds = session.client('rds-data', region_name='us-west-2')
-
-    sql = "insert into Hello (message) values ('hello from local machine')"
-
-    response = rds.execute_statement(
-        resourceArn=os.getenv("CLUSTER_ARN"),
-        secretArn=os.getenv("SECRET_ARN"),
-        database='hello',
-        sql=sql
-    )
-    print(str(response))
-
-
-def bucket(session):
-    s3 = session.resource('s3')
-
-    foo = pd.DataFrame({'x': [1, 2, 3], 'y': ['a', 'b', 'c']})
-    # don't export to file, return csv string
-    csv_str = foo.to_csv()
-    # put_object method requires byte stream, not string
-    csv_stream = bytes(csv_str.encode('UTF-8'))
-
-    s3.Bucket(os.getenv("S3_BUCKET")).put_object(
-        Key='testing.csv', Body=csv_stream)
+from Model import Model
+from DB import DB
+from NBA import NBA
+from Storage import Storage
 
 
 def driver_fn(event=None, context=None):
+
     # LAMBDA_TASK_ROOT environment variable does not exist locally
     is_lambda = os.environ.get('LAMBDA_TASK_ROOT')
 
@@ -53,10 +23,18 @@ def driver_fn(event=None, context=None):
         # on lambda it's using the role assigned to the Lambda function
         session = boto3.session.Session()
 
-    bucket(session)
-    db(session)
+    nba = NBA()
+    db = DB(session)
+    model = Model()
+    storage = Storage(session)
+
+    boxscores = nba.iterate_games(nba.get_boxscore)
+    db.dataframe_to_sql(boxscores)
+    # get relevant data from db for model
+    model_data = db.sql_to_dataframe('', None)
+    model_outcome = model.model_fn(model_data)
+    storage.to_csv(model_outcome, '')
 
 
 if __name__ == "__main__":
-    print(os.getenv("S3_BUCKET"))
     driver_fn()
