83cd1d635e24e2547e312eda367b868908b9717b
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-09-28 13:00:19 -0700

    Implement DB superclass, and have auroradb and localdb inherit from it

--- db.py
+++ db.py
@@ -1,32 +1,15 @@
 import os
-
-# this will be used to access Aurora's Data API
-from sqlalchemy import create_engine
-# awswrangler provides convenient methods for Pandas to AWS DB
-import awswrangler as wr
+from abc import ABC, abstractmethod
 
 
-class DB:
-    def __init__(self, session):
+class DB(ABC):
+    def __init__(self):
         pass
 
+    @abstractmethod
     def dataframe_to_sql(self, df):
         pass
 
-    # old code
-    def insert(self, session):
-        rds = session.client('rds-data', region_name='us-west-2')
-
-        sql = "insert into Hello (message) values ('hello from local machine')"
-
-        response = rds.execute_statement(
-            resourceArn=os.getenv("CLUSTER_ARN"),
-            secretArn=os.getenv("SECRET_ARN"),
-            database='hello',
-            sql=sql
-        )
-        print(str(response))
-
+    @abstractmethod
     def sql_to_dataframe(self, query, table):
-        # use aws wrangler read_sql_query to read into dataframe
         pass
---
+++ localdb.py
@@ -0,0 +1,21 @@
+import os
+
+import mysql.connector
+
+from db import DB
+
+
+class LocalDB(DB):
+    def __init__(self):
+        DB.__init__(self)
+        self.connection = mysql.connector.connect(
+            host=os.getenv("HOST"),
+            user=os.getenv("USER"),
+            database=os.getenv('DATABASE'),
+            password=os.getenv('PASSWORD'))
+
+    def dataframe_to_sql(self, df):
+        pass
+
+    def sql_to_dataframe(self, query, table):
+        pass
