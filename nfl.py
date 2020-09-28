de377e6ffaa8279014dedf914d839b4095025de5
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-09-28 14:07:17 -0700

    Use date.fromisoformat()

--- nfl.py
+++ nfl.py
@@ -1,15 +1,14 @@
 from datetime import date, timedelta, datetime
 
-from sportsreference.nfl.boxscore import Boxscore
+from sportsreference.nfl.boxscore import Boxscore, Boxscores
 
 from sport import Sport
 
 
 class NFL(Sport):
     def __init__(self):
-        datetime.strptime('2020-09-10', '%Y-%m-%d').date()
-        Sport.__init__(self, start_date=datetime.strptime(
-            '2020-09-10', '%Y-%m-%d').date())
+        Sport.__init__(self, start_date=date.fromisoformat(
+            '2020-09-10'), season=2020)
 
     def _week_from_date(self, date_):
         return int((date_ - self.start_date).days / 7) + 1
@@ -18,6 +17,8 @@ class NFL(Sport):
         pass
 
     def get_boxscores(self, date_):
-        pass
-
         # get the week from the date
+        week = self._week_from_date(date_)
+        games = Boxscores(week, self.season)
+
+        return True
--- test/test_NFL.py
+++ test/test_NFL.py
@@ -12,5 +12,9 @@ def nfl():
 
 @pytest.mark.parametrize('date_str, week', [('2020-09-10', 1), ('2020-09-11', 1), ('2020-09-19', 2), ('2021-01-03', 17)])
 def test_week_from_date(nfl, date_str, week):
-    date_ = datetime.strptime(date_str, '%Y-%m-%d').date()
-    assert nfl._week_from_date(date_) == week
+    assert nfl._week_from_date(date.fromisoformat(date_str)) == week
+
+
+@pytest.mark.parametrize('date_str, expected', [('2020-09-10', True)])
+def test_get_boxscores(nfl, date_str, expected):
+    assert nfl.get_boxscores(date.fromisoformat(date_str)) == expected
