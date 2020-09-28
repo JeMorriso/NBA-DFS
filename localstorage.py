eae25bd463403c6e7521dbda5820e22f9aa6bebd
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-09-28 13:42:40 -0700

    Define Storage interface, have LocalStorage, S3Storage implement

---
+++ localstorage.py
@@ -0,0 +1,14 @@
+
+import os
+
+import pandas as pd
+
+from storage import Storage
+
+
+class LocalStorage(Storage):
+    def __init__(self):
+        pass
+
+    def to_csv(self, df, path):
+        pass
--- storage.py
+++ storage.py
@@ -1,27 +1,10 @@
-import os
-
-import pandas as pd
+from abc import ABC, abstractmethod
 
 
 class Storage:
-    def __init__(self, session, bucket=None):
-        # boto3 session from user or lambda role
-        self.s3 = session.resource('s3')
+    def __init__(self):
+        pass
 
-        if bucket is None:
-            self.bucket = os.getenv('S3_BUCKET')
-        else:
-            self.bucket = bucket
-
+    @abstractmethod
     def to_csv(self, df, path):
         pass
-        # dump model generated rosters to csv
-
-        # foo = pd.DataFrame({'x': [1, 2, 3], 'y': ['a', 'b', 'c']})
-        # # don't export to file, return csv string
-        # csv_str = foo.to_csv()
-        # # put_object method requires byte stream, not string
-        # csv_stream = bytes(csv_str.encode('UTF-8'))
-
-        # s3.Bucket(os.getenv("S3_BUCKET")).put_object(
-        #     Key='testing.csv', Body=csv_stream)
