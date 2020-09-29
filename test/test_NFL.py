3723f8351e520ba615b274f87a0525f01b328703
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-09-28 19:44:19 -0700

    Fix NFL tests for get_boxscores() and get_players_game_stats()

--- test/test_NFL.py
+++ test/test_NFL.py
@@ -15,12 +15,29 @@ def test_week_from_date(nfl, date_str, week):
     assert nfl._week_from_date(date.fromisoformat(date_str)) == week
 
 
-@pytest.mark.parametrize('date_str, expected', [('2020-09-10', True)])
-def test_get_boxscores(nfl, date_str, expected):
-    assert nfl.get_boxscores(date.fromisoformat(date_str)) == expected
+@pytest.mark.parametrize('date_str, num_games', [('2020-09-10', 16)])
+def test_get_boxscores(nfl, date_str, num_games):
+    date_ = date.fromisoformat(date_str)
+    boxscores = nfl.get_boxscores(date_)
+    assert len(
+        boxscores.games[f'{nfl._week_from_date(date_)}-{nfl.season}']) == num_games
 
 
-@pytest.mark.parametrize('date_str, expected', [('2020-09-10', True)])
-def test_get_players_game_stats(nfl, date_str, expected):
-    assert nfl.get_players_game_stats(
-        date.fromisoformat(date_str)) == expected
+@pytest.mark.parametrize('date_str', [('2020-09-10')])
+def test_get_players_game_stats(nfl, date_str):
+    df = nfl.get_players_game_stats(
+        date.fromisoformat(date_str))
+
+    assert len(df) > 1
+    # + 1 because of sportsreference_id
+    assert len(df.columns) == len(nfl.categories) + 1
+
+
+@pytest.mark.parametrize('date_str', [('2020-09-10')])
+def test_get_games_info(nfl, date_str):
+    df = nfl.get_games_info(
+        date.fromisoformat(date_str))
+
+    assert len(df) > 1
+    # + 1 because of sportsreference_id
+    assert len(df.columns) == len(nfl.categories) + 1
