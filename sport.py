1d94cef7d52082859bdd02f6733a2d19179701cb
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-01 00:30:33 -0700

    Change sport classes so that they hold sport-specific configuration only

--- sport.py
+++ sport.py
@@ -1,17 +1,7 @@
-from abc import ABC, abstractmethod
-
-
 class Sport:
-    def __init__(self, start_date=None, season=None, categories=None):
+    def __init__(self, start_date, season, categories, positions):
         self.start_date = start_date
         # self.end_date = None
         self.season = season
         self.categories = categories
-
-    @abstractmethod
-    def get_boxscore(self, id):
-        pass
-
-    @abstractmethod
-    def get_boxscores(self, date_):
-        pass
+        self.positions = positions
