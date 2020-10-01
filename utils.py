d9f9521e990feb01100f22fdd2251687ce257699
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-01 00:29:51 -0700

    Add utility methods

---
+++ utils.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+
+class Utils:
+    @classmethod
+    def _week_from_date(cls, date_, start_date):
+        return int((date_ - start_date).days / 7) + 1
+
+    @classmethod
+    def _date_to_sportsreference_str(cls, date_):
+        return date_.strftime("%m-%d-%Y")
