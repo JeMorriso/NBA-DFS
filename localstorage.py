8d976fe8f2247d18e6a55249549e74e8af5e094a
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-02 19:48:20 -0700

    Change storage interface and implement localstorage

--- localstorage.py
+++ localstorage.py
@@ -1,4 +1,3 @@
-
 import os
 
 import pandas as pd
@@ -8,7 +7,10 @@ from storage import Storage
 
 class LocalStorage(Storage):
     def __init__(self):
-        pass
+        self.dir = os.getenv("LOCAL_DIR")
 
-    def to_csv(self, df, path):
-        pass
+    def csv_to_dataframe(self, path):
+        return pd.read_csv(self.dir + path)
+
+    def dataframe_to_csv(self, df, path):
+        df.to_csv(self.dir + path)
--- storage.py
+++ storage.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 
 
-class Storage:
+class Storage(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
-    def to_csv(self, df, path):
+    def dataframe_to_csv(self, df, path):
+        pass
+
+    @abstractmethod
+    def csv_to_dataframe(self, path):
         pass
