e4852ef2cc06b15c1f055ebcfd15440b1c77382f
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-05 22:46:40 -0700

    Add TimeInterval class, and change date range method to use intervals

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+from datetime import timedelta
+from enum import Enum
+import math
+
+
+class TimeInterval(Enum):
+    DAY = 1
+    WEEK = 7
+
+
 class Utils:
     @classmethod
     def _week_from_date(cls, date_, start_date):
@@ -6,3 +16,10 @@ class Utils:
     @classmethod
     def _date_to_sportsreference_str(cls, date_):
         return date_.strftime("%m-%d-%Y")
+
+    @classmethod
+    def _date_range(cls, start_date, end_date, interval=TimeInterval.DAY):
+        return [
+            start_date + timedelta(i) * interval.value
+            for i in range(math.ceil((end_date - start_date).days / interval.value))
+        ]
