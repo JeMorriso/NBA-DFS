63ceaf0f0ad05c38cbbd728dda75f0962c575e0c
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-10-13 14:52:06 -0700

    Update database tests to include tests for AuroraDB

--- test/test_db.py
+++ test/test_db.py
@@ -1,46 +1,33 @@
-import os
-
 import pytest
 import pandas as pd
-from sqlalchemy.exc import IntegrityError
-
-from localdb import LocalDB
-from nflsportsreference import NFLSportsReference
-from nfl import NFL
+from sqlalchemy.exc import IntegrityError, DatabaseError
 
 
-@pytest.fixture
-def nfl_local():
-    return LocalDB(os.getenv("NFL_DATABASE"))
+@pytest.mark.parametrize("db", [("local"), ("aurora")], indirect=["db"])
+def test_to_dataframe(db):
+    df = db.sql_to_dataframe("select * from team")
+    assert len(df) == 32
 
 
-@pytest.fixture
-def nfl():
-    return NFLSportsReference(NFL())
-
-
-def test_nfl_local_init(nfl_local):
-    pass
-
-
-def test_to_dataframe(nfl_local):
-    df = nfl_local.sql_to_dataframe("select * from teams")
-    print(df.head())
-
-
-def test_to_sql_duplicate(nfl_local):
-    d = {"sportsreference_abbreviation": ["htx"], "name": ["Houston Texans"]}
+@pytest.mark.parametrize("db", [("local"), ("aurora")], indirect=["db"])
+def test_to_sql_duplicate(db):
+    d = {"abbreviation": ["htx"], "name": ["Houston Texans"]}
     df = pd.DataFrame(data=d)
     try:
-        nfl_local.dataframe_to_sql(df, "team", index=False, if_exists="append")
-        nfl_local.dataframe_to_sql(df, "team", index=False, if_exists="append")
+        db.dataframe_to_sql(df, "team", index=False, if_exists="append")
+        db.dataframe_to_sql(df, "team", index=False, if_exists="append")
         pytest.fail("should not insert duplicate")
     except IntegrityError:
         pass
+    except DatabaseError:
+        pass
 
 
-def test_insert_teams(nfl_local, nfl):
+@pytest.mark.parametrize(
+    "db, nfl", [("local", "week"), ("aurora", "week")], indirect=["db", "nfl"]
+)
+def test_insert_teams(db, nfl):
     df = nfl.get_teams_info()
-    nfl_local.dataframe_to_sql(df, "team", index=False)
-    df2 = nfl_local.sql_to_dataframe("select * from team")
+    db.dataframe_to_sql(df, "team", index=False)
+    df2 = db.sql_to_dataframe("select * from team")
     assert len(df) == len(df2)
