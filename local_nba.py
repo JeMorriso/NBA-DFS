f98414325c359c96ff71936abca59cc999b8dbf9
Jeremy Morrison <jeremy.morrison36@gmail.com>
2021-01-11 21:09:28 -0800

    Move old files to aws branch

--- local_nba.py
+++
@@ -1,8 +0,0 @@
-from local_pipeline import driver_fn
-from model import Model
-from localdb import LocalDB
-from nba import NBA
-from localstorage import LocalStorage
-
-if __name__ == "__main__":
-    driver_fn(NBA(), LocalDB(), Model(), LocalStorage())
